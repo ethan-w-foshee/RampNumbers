8cdafa89ce9d5ed572cd6ab1482b275822771ebe
ethan.w.foshee <skyhailer2@gmail.com>
2022-12-01 19:36:20 -0600

    Main Logic Functionality seems to work

--- main.js
+++ main.js
@@ -4,13 +4,14 @@ function rampNumbers(num) {
     let testing2 = 0
     for(let i = 0; i < num.length; i++) {
         testing1 = parseInt(num[i])
-        testing2 = parseInt(num[i-1])
-        console.log(num[i])
+        testing2 = parseInt(num[i+1])
+        console.log("Number 1 = " + num[i] + " Number 2 = " + num[i+1])
         if(testing1 > testing2){
-
+            return false
+            break;
         }
         else {
-            return false
+
         }
     }
     return true
